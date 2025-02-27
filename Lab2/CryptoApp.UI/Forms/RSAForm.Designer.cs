6a0441fe524bbe35ffd411885e3a607991da4285
Nikita Lolenko <loleenko@vk.com>
2025-02-27 21:27:23 +0300

    фф

--- Lab2/CryptoApp.UI/Forms/RSAForm.Designer.cs
+++ Lab2/CryptoApp.UI/Forms/RSAForm.Designer.cs
@@ -25,130 +25,148 @@ namespace Lab2
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
-            
             SuspendLayout();
-            
+            // 
             // textBoxInput
-            textBoxInput.Location = new Point(12, 32);
+            // 
+            textBoxInput.BackColor = Color.White;
+            textBoxInput.BorderStyle = BorderStyle.FixedSingle;
+            textBoxInput.Font = new Font("Segoe UI", 9F);
+            textBoxInput.ForeColor = Color.FromArgb(64, 64, 64);
+            textBoxInput.Location = new Point(14, 43);
+            textBoxInput.Margin = new Padding(3, 4, 3, 4);
             textBoxInput.Multiline = true;
             textBoxInput.Name = "textBoxInput";
             textBoxInput.ScrollBars = ScrollBars.Vertical;
-            textBoxInput.Size = new Size(637, 60);
+            textBoxInput.Size = new Size(728, 79);
             textBoxInput.TabIndex = 0;
-            textBoxInput.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            textBoxInput.BackColor = Color.White;
-            textBoxInput.ForeColor = Color.FromArgb(64, 64, 64);
-            textBoxInput.BorderStyle = BorderStyle.FixedSingle;
-            
+            // 
             // textBoxEncrypted
-            textBoxEncrypted.Location = new Point(12, 118);
+            // 
+            textBoxEncrypted.BackColor = Color.FromArgb(245, 245, 245);
+            textBoxEncrypted.BorderStyle = BorderStyle.FixedSingle;
+            textBoxEncrypted.Font = new Font("Segoe UI", 9F);
+            textBoxEncrypted.ForeColor = Color.FromArgb(64, 64, 64);
+            textBoxEncrypted.Location = new Point(14, 157);
+            textBoxEncrypted.Margin = new Padding(3, 4, 3, 4);
             textBoxEncrypted.Multiline = true;
             textBoxEncrypted.Name = "textBoxEncrypted";
             textBoxEncrypted.ScrollBars = ScrollBars.Vertical;
-            textBoxEncrypted.Size = new Size(637, 60);
+            textBoxEncrypted.Size = new Size(728, 79);
             textBoxEncrypted.TabIndex = 1;
-            textBoxEncrypted.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            textBoxEncrypted.BackColor = Color.FromArgb(245, 245, 245);
-            textBoxEncrypted.ForeColor = Color.FromArgb(64, 64, 64);
-            textBoxEncrypted.BorderStyle = BorderStyle.FixedSingle;
-            
+            // 
             // textBoxDecrypted
-            textBoxDecrypted.Location = new Point(12, 204);
+            // 
+            textBoxDecrypted.BackColor = Color.FromArgb(245, 245, 245);
+            textBoxDecrypted.BorderStyle = BorderStyle.FixedSingle;
+            textBoxDecrypted.Font = new Font("Segoe UI", 9F);
+            textBoxDecrypted.ForeColor = Color.FromArgb(64, 64, 64);
+            textBoxDecrypted.Location = new Point(14, 272);
+            textBoxDecrypted.Margin = new Padding(3, 4, 3, 4);
             textBoxDecrypted.Multiline = true;
             textBoxDecrypted.Name = "textBoxDecrypted";
             textBoxDecrypted.ReadOnly = true;
             textBoxDecrypted.ScrollBars = ScrollBars.Vertical;
-            textBoxDecrypted.Size = new Size(637, 60);
+            textBoxDecrypted.Size = new Size(728, 79);
             textBoxDecrypted.TabIndex = 2;
-            textBoxDecrypted.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            textBoxDecrypted.BackColor = Color.FromArgb(245, 245, 245);
-            textBoxDecrypted.ForeColor = Color.FromArgb(64, 64, 64);
-            textBoxDecrypted.BorderStyle = BorderStyle.FixedSingle;
-            
+            // 
             // buttonEncrypt
-            buttonEncrypt.Location = new Point(12, 270);
+            // 
+            buttonEncrypt.BackColor = Color.FromArgb(0, 120, 215);
+            buttonEncrypt.Cursor = Cursors.Hand;
+            buttonEncrypt.FlatAppearance.BorderColor = Color.FromArgb(0, 120, 215);
+            buttonEncrypt.FlatAppearance.MouseDownBackColor = Color.FromArgb(204, 228, 247);
+            buttonEncrypt.FlatAppearance.MouseOverBackColor = Color.FromArgb(229, 241, 251);
+            buttonEncrypt.FlatStyle = FlatStyle.Flat;
+            buttonEncrypt.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            buttonEncrypt.ForeColor = Color.White;
+            buttonEncrypt.Location = new Point(14, 360);
+            buttonEncrypt.Margin = new Padding(3, 4, 3, 4);
             buttonEncrypt.Name = "buttonEncrypt";
-            buttonEncrypt.Size = new Size(200, 30);
+            buttonEncrypt.Size = new Size(229, 40);
             buttonEncrypt.TabIndex = 3;
             buttonEncrypt.Text = "Зашифровать";
-            buttonEncrypt.FlatStyle = FlatStyle.Flat;
-            buttonEncrypt.FlatAppearance.BorderColor = Color.FromArgb(0, 120, 215);
-            buttonEncrypt.FlatAppearance.MouseOverBackColor = Color.FromArgb(229, 241, 251);
-            buttonEncrypt.FlatAppearance.MouseDownBackColor = Color.FromArgb(204, 228, 247);
-            buttonEncrypt.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            buttonEncrypt.BackColor = Color.FromArgb(0, 120, 215);
-            buttonEncrypt.ForeColor = Color.White;
-            buttonEncrypt.Cursor = Cursors.Hand;
+            buttonEncrypt.UseVisualStyleBackColor = false;
             buttonEncrypt.Click += buttonEncrypt_Click;
-            
+            // 
             // buttonDecrypt
-            buttonDecrypt.Location = new Point(218, 270);
+            // 
+            buttonDecrypt.BackColor = Color.White;
+            buttonDecrypt.Cursor = Cursors.Hand;
+            buttonDecrypt.FlatAppearance.BorderColor = Color.FromArgb(0, 120, 215);
+            buttonDecrypt.FlatAppearance.MouseDownBackColor = Color.FromArgb(204, 228, 247);
+            buttonDecrypt.FlatAppearance.MouseOverBackColor = Color.FromArgb(229, 241, 251);
+            buttonDecrypt.FlatStyle = FlatStyle.Flat;
+            buttonDecrypt.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            buttonDecrypt.ForeColor = Color.FromArgb(0, 120, 215);
+            buttonDecrypt.Location = new Point(249, 360);
+            buttonDecrypt.Margin = new Padding(3, 4, 3, 4);
             buttonDecrypt.Name = "buttonDecrypt";
-            buttonDecrypt.Size = new Size(200, 30);
+            buttonDecrypt.Size = new Size(229, 40);
             buttonDecrypt.TabIndex = 4;
             buttonDecrypt.Text = "Расшифровать";
-            buttonDecrypt.FlatStyle = FlatStyle.Flat;
-            buttonDecrypt.FlatAppearance.BorderColor = Color.FromArgb(0, 120, 215);
-            buttonDecrypt.FlatAppearance.MouseOverBackColor = Color.FromArgb(229, 241, 251);
-            buttonDecrypt.FlatAppearance.MouseDownBackColor = Color.FromArgb(204, 228, 247);
-            buttonDecrypt.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            buttonDecrypt.BackColor = Color.White;
-            buttonDecrypt.ForeColor = Color.FromArgb(0, 120, 215);
-            buttonDecrypt.Cursor = Cursors.Hand;
+            buttonDecrypt.UseVisualStyleBackColor = false;
             buttonDecrypt.Click += buttonDecrypt_Click;
-            
+            // 
             // labelPublicKey
+            // 
             labelPublicKey.AutoSize = true;
-            labelPublicKey.Location = new Point(12, 310);
-            labelPublicKey.Name = "labelPublicKey";
-            labelPublicKey.Size = new Size(200, 15);
-            labelPublicKey.TabIndex = 5;
-            labelPublicKey.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            labelPublicKey.Font = new Font("Segoe UI", 9F);
             labelPublicKey.ForeColor = Color.FromArgb(64, 64, 64);
-            
+            labelPublicKey.Location = new Point(14, 413);
+            labelPublicKey.Name = "labelPublicKey";
+            labelPublicKey.Size = new Size(0, 20);
+            labelPublicKey.TabIndex = 5;
+            // 
             // labelPrivateKey
+            // 
             labelPrivateKey.AutoSize = true;
-            labelPrivateKey.Location = new Point(12, 335);
-            labelPrivateKey.Name = "labelPrivateKey";
-            labelPrivateKey.Size = new Size(200, 15);
-            labelPrivateKey.TabIndex = 6;
-            labelPrivateKey.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            labelPrivateKey.Font = new Font("Segoe UI", 9F);
             labelPrivateKey.ForeColor = Color.FromArgb(64, 64, 64);
-            
+            labelPrivateKey.Location = new Point(14, 447);
+            labelPrivateKey.Name = "labelPrivateKey";
+            labelPrivateKey.Size = new Size(0, 20);
+            labelPrivateKey.TabIndex = 6;
+            // 
             // label1
+            // 
             label1.AutoSize = true;
-            label1.Location = new Point(12, 12);
+            label1.Font = new Font("Segoe UI", 9F);
+            label1.ForeColor = Color.FromArgb(64, 64, 64);
+            label1.Location = new Point(14, 16);
             label1.Name = "label1";
-            label1.Size = new Size(200, 15);
+            label1.Size = new Size(164, 20);
             label1.TabIndex = 7;
             label1.Text = "Исходное сообщение:";
-            label1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.ForeColor = Color.FromArgb(64, 64, 64);
-            
+            // 
             // label2
+            // 
             label2.AutoSize = true;
-            label2.Location = new Point(12, 98);
+            label2.Font = new Font("Segoe UI", 9F);
+            label2.ForeColor = Color.FromArgb(64, 64, 64);
+            label2.Location = new Point(14, 131);
             label2.Name = "label2";
-            label2.Size = new Size(200, 15);
+            label2.Size = new Size(212, 20);
             label2.TabIndex = 8;
             label2.Text = "Зашифрованное сообщение:";
-            label2.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.ForeColor = Color.FromArgb(64, 64, 64);
-            
+            // 
             // label3
+            // 
             label3.AutoSize = true;
-            label3.Location = new Point(12, 184);
+            label3.Font = new Font("Segoe UI", 9F);
+            label3.ForeColor = Color.FromArgb(64, 64, 64);
+            label3.Location = new Point(14, 245);
             label3.Name = "label3";
-            label3.Size = new Size(200, 15);
+            label3.Size = new Size(219, 20);
             label3.TabIndex = 9;
             label3.Text = "Расшифрованное сообщение:";
-            label3.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            label3.ForeColor = Color.FromArgb(64, 64, 64);
-            
+            // 
             // RSAForm
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(661, 408);
+            BackColor = Color.White;
+            ClientSize = new Size(755, 544);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
@@ -159,11 +177,33 @@ namespace Lab2
             Controls.Add(textBoxDecrypted);
             Controls.Add(textBoxEncrypted);
             Controls.Add(textBoxInput);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "RSAForm";
             Text = "RSA";
-            BackColor = Color.White;
+            Controls.Add(buttonInsertTestData);
+
             ResumeLayout(false);
             PerformLayout();
+            // 
+            // buttonInsertTestData
+            // 
+            buttonInsertTestData = new Button();
+            buttonInsertTestData.BackColor = Color.FromArgb(0, 120, 215);
+            buttonInsertTestData.Cursor = Cursors.Hand;
+            buttonInsertTestData.FlatAppearance.BorderColor = Color.FromArgb(0, 120, 215);
+            buttonInsertTestData.FlatAppearance.MouseDownBackColor = Color.FromArgb(204, 228, 247);
+            buttonInsertTestData.FlatAppearance.MouseOverBackColor = Color.FromArgb(229, 241, 251);
+            buttonInsertTestData.FlatStyle = FlatStyle.Flat;
+            buttonInsertTestData.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            buttonInsertTestData.ForeColor = Color.White;
+            buttonInsertTestData.Location = new Point(488, 360); // выберите удобное положение
+            buttonInsertTestData.Margin = new Padding(3, 4, 3, 4);
+            buttonInsertTestData.Name = "buttonInsertTestData";
+            buttonInsertTestData.Size = new Size(254, 40);
+            buttonInsertTestData.TabIndex = 10;
+            buttonInsertTestData.Text = "Вставить тестовые данные";
+            buttonInsertTestData.UseVisualStyleBackColor = false;
+            buttonInsertTestData.Click += buttonInsertTestData_Click;
         }
 
         private TextBox textBoxInput;
@@ -176,5 +216,7 @@ namespace Lab2
         private Label label1;
         private Label label2;
         private Label label3;
+        private Button buttonInsertTestData;
+
     }
 } 
